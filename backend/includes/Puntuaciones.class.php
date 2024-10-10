d4688ed85aa61004bccaa5f626ed120fe6009c06
Jorge Iriarte <106839094+joririarte@users.noreply.github.com>
2024-10-10 16:46:31 -0300

    metodos add y get para puntuaciones

--- backend/includes/Puntuaciones.class.php
+++ backend/includes/Puntuaciones.class.php
@@ -1,130 +1,56 @@
 <?php
     require_once('Database.class.php');
-    class Puntuaciones{//hay que modificar todo para puntuaciones
-        public static function AddPregunta($texto) {
+    require_once('JsonFormatter.class.php');
+    class Puntuaciones{
+        public static function AddPuntuacion($id_usuario, $id_cuestionario, $puntaje, $tiempo) {
             try {
                 $database = new Database();
                 $conn = $database->getConnection();
         
-                $stmt = $conn->prepare('CALL AddPregunta(:texto)');
-                $stmt->bindParam(':texto', $texto);
+                $stmt = $conn->prepare('CALL AddPuntuacion(:id_usuario, :id_cuestionario, :puntaje, :tiempo)');
+                $stmt->bindParam(':id_usuario', $id_usuario);
+                $stmt->bindParam(':id_cuestionario', $id_cuestionario);
+                $stmt->bindParam(':puntaje', $puntaje);
+                $stmt->bindParam(':tiempo', $tiempo);
         
                 if ($stmt->execute()) {
-                    header('HTTP/1.1 201 Pregunta creada correctamente');
+                    JsonFormatter::printJsonAnswer('HTTP/1.1 201 OK', ['success' => 'Puntuación creada correctamente']);
                 } else {
                     // Obtener el error de la base de datos
                     $errorInfo = $stmt->errorInfo();
                     $errorMessage = $errorInfo[2]; // Mensaje de error de la base de datos
         
-                    // Enviar el error como parte de la respuesta HTTP
-                    header('HTTP/1.1 500 Error al crear la Pregunta');
-                    echo json_encode(['error' => $errorMessage]);
+                    JsonFormatter::printJsonAnswer('HTTP/1.1 500 Error al crear la Puntuación', ['error' => $errorMessage]);
                 }
             } catch (PDOException $e) {
-                // Captura de excepciones y manejo de errores de PDO
-                header('HTTP/1.1 500 Error del servidor');
-                echo json_encode(['error' => $e->getMessage()]);
+                JsonFormatter::printJsonAnswer('HTTP/1.1 500 Error del servidor', ['error' => $e->getMessage()]);
             }
         }
         
 
-        public static function GetAllPreguntas() {
+        public static function GetAllPuntuaciones() {
             try {
                 $database = new Database();
                 $conn = $database->getConnection();
         
-                $stmt = $conn->prepare('CALL GetAllPreguntas()');
+                $stmt = $conn->prepare('CALL GetAllPuntuaciones()');
         
                 if ($stmt->execute()) {
-                    $result = $stmt->fetchAll();
-                    echo json_encode($result);
-                    header('HTTP/1.1 200 OK');
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    if (!empty($result)) {
+                        JsonFormatter::printJsonAnswer('HTTP/1.1 200 OK', $result);
+                    } else {
+                        JsonFormatter::printJsonAnswer('HTTP/1.1 404 Puntuaciones no encontradas',['error' => 'Puntuaciones no encontrados']);
+                    }
                 } else {
                     // Obtener y mostrar el error de la base de datos
                     $errorInfo = $stmt->errorInfo();
                     $errorMessage = $errorInfo[2];
-                    header('HTTP/1.1 404 No válido');
-                    echo json_encode(['error' => $errorMessage]);
+                    JsonFormatter::printJsonAnswer('HTTP/1.1 404 Puntuaciones no válidas', ['error' => $errorMessage]);
                 }
             } catch (PDOException $e) {
-                header('HTTP/1.1 500 Error del servidor');
-                echo json_encode(['error' => $e->getMessage()]);
+                JsonFormatter::printJsonAnswer('HTTP/1.1 500 Error del servidor', ['error' => $e->getMessage()]);
             }
-        }
-
-        public static function GetPregunta($id) {
-            try {
-                $database = new Database();
-                $conn = $database->getConnection();
-        
-                $stmt = $conn->prepare('CALL GetPregunta(:id)');
-                $stmt->bindParam(':id', $id);
-        
-                if ($stmt->execute()) {
-                    $result = $stmt->fetchAll();
-                    echo json_encode($result);
-                    header('HTTP/1.1 200 OK');
-                } else {
-                    // Obtener y mostrar el error de la base de datos
-                    $errorInfo = $stmt->errorInfo();
-                    $errorMessage = $errorInfo[2];
-                    header('HTTP/1.1 404 No válido');
-                    echo json_encode(['error' => $errorMessage]);
-                }
-            } catch (PDOException $e) {
-                header('HTTP/1.1 500 Error del servidor');
-                echo json_encode(['error' => $e->getMessage()]);
-            }
-        }
-        
-
-        public static function DeletePregunta($id) {
-            try {
-                $database = new Database();
-                $conn = $database->getConnection();
-        
-                $stmt = $conn->prepare('CALL DeletePregunta(:id)');
-                $stmt->bindParam(':id', $id);
-        
-                if ($stmt->execute()) {
-                    header('HTTP/1.1 200 Pregunta borrada correctamente');
-                } else {
-                    // Obtener y mostrar el error de la base de datos
-                    $errorInfo = $stmt->errorInfo();
-                    $errorMessage = $errorInfo[2];
-                    header('HTTP/1.1 404 Pregunta no se ha borrado correctamente');
-                    echo json_encode(['error' => $errorMessage]);
-                }
-            } catch (PDOException $e) {
-                header('HTTP/1.1 500 Error del servidor');
-                echo json_encode(['error' => $e->getMessage()]);
-            }
-        }
-        
-
-        public static function UpdatePregunta($id, $texto) {
-            try {
-                $database = new Database();
-                $conn = $database->getConnection();
-        
-                $stmt = $conn->prepare('CALL UpdateUser(:id, :texto)');
-                $stmt->bindParam(':id', $id);
-                $stmt->bindParam(':texto', $texto);
-        
-                if ($stmt->execute()) {
-                    header('HTTP/1.1 200 Pregunta actualizada correctamente');
-                } else {
-                    // Obtener y mostrar el error de la base de datos
-                    $errorInfo = $stmt->errorInfo();
-                    $errorMessage = $errorInfo[2];
-                    header('HTTP/1.1 404 Pregunta no se ha actualizado correctamente');
-                    echo json_encode(['error' => $errorMessage]);
-                }
-            } catch (PDOException $e) {
-                header('HTTP/1.1 500 Error del servidor');
-                echo json_encode(['error' => $e->getMessage()]);
-            }
-        }
-        
+        }     
     }
 ?>
