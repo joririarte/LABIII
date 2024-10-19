fb02b061f34f92de5b25ba679b9cd44185a38474
Jorge Iriarte <106839094+joririarte@users.noreply.github.com>
2024-10-18 22:54:02 -0300

    Add files via upload

--- backend/includes/Database.class.php
+++ backend/includes/Database.class.php
@@ -1,16 +1,19 @@
 <?php
     class Database{
         private $host = 'localhost';
-        private $user = 'user';
-        private $password = 'pass';
-        private $database = 'db';
+        private $user = 'buenamaravilla_db_user';
+        private $password = '#Li64Jo10Ri25Sa73#';
+        private $database = 'buenamaravilla_trivia_dev';
     
         public function getConnection(){
-            $hostDB = "mysql:host=".$this->host.";dbname=".$this->database.";";
+            $hostDB = "mysql:host=".$this->host.";dbname=".$this->database.";charset=utf8mb4";
 
             try{
-                $connection = new PDO($hostDB, $this->user, $this->password);
-                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $options = [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true // Habilitar el buffering
+                ];
+                $connection = new PDO($hostDB, $this->user, $this->password, $options);
                 return $connection;
             } catch(PDOException $e){
                 die("ERROR: ".$e->getMessage());
